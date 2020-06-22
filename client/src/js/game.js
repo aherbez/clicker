db22c6cde97df0f2e895737b2eef9f10a7750e74
aherbez <aherbez@gmail.com>
2020-06-22 11:15:26 -0700

    pulling dimensions from canvas

--- client/src/js/game.js
+++ client/src/js/game.js
@@ -7,17 +7,23 @@ export class ClickerClient {
         this.canvasEl = document.getElementById(elId);
         this.ctx = this.canvasEl.getContext('2d');
 
+        this.bounds = {
+            width: this.canvasEl.clientWidth,
+            height: this.canvasEl.clientHeight,
+        }
+
         this.render();
     }
 
     render() {
         let c = this.ctx;
+        let { width, height } = this.bounds;        
 
-        c.clearRect(0, 0, WIDTH, HEIGHT);
+        c.clearRect(0, 0, width, height);
 
 
         c.save();
-        c.translate(WIDTH/2, HEIGHT/2);
+        c.translate(width/2, height/2);
         c.fillRect(-10, -10, 20, 20);
         c.restore();
 
